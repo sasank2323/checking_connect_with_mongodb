ad91f38d055a3272e01c98329be3e0965459c889
sasank <saisasank2323@gmail.com>
2025-03-29 20:08:34 +0530

    Breaking it Down

--- src/main/java/com/journal/App/AppApplication.java
+++ src/main/java/com/journal/App/AppApplication.java
@@ -2,12 +2,24 @@ package com.journal.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	//interface -- platform transaction manger ,implementation -mongo transaction manager
+	// it have commit ,roll back etc..,
+	@Bean
+	public PlatformTransactionManager falana(MongoDatabaseFactory dbFactory){
+		return new MongoTransactionManager(dbFactory);
+	}
 }
